22eb4cf1bf73b1b1c0ea6dd5cd2c366777eebb0a
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-01-06 18:18:46 +0900

    feat: 회원가입 router 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router"
 import Login from '@/views/Login.vue'
 import companyRoutes from "./company"
 import userRoutes from "./user"
+import Signup from '@/views/Signup.vue'
 
 import CompanyCoupon from '@/views/company/CompanyCoupon.vue'
 
@@ -13,6 +14,11 @@ const routes = [
         name: 'login',
         component: Login
     },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
+    }
 ]
 
 const router = createRouter({
