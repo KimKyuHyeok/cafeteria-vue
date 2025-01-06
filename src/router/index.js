e1765ce937182360d13ee58f3bfefd0a7d4652cb
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-01-06 12:37:54 +0900

    fix: router 검증 임시 구현

--- src/router/index.js
+++ src/router/index.js
@@ -20,4 +20,20 @@ const router = createRouter({
     routes
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) { // Boolean 값이 true일 때
+    const token = localStorage.getItem('companyToken') || localStorage.getItem('userToken'); // 적절한 토큰 확인
+
+    if (!token) {
+      next('/login'); // 인증되지 않은 경우 로그인 페이지로 리다이렉트
+    } else {
+      next(); // 인증된 경우 페이지 접근 허용
+    }
+  } else {
+    next(); // requiresAuth가 없거나 false일 경우 접근 허용
+  }
+});
+
+  
+
 export default router
