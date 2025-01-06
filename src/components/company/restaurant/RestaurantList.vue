6ed47b8aa318b7acd84ab1c07b8c6d89fa136b4f
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-01-06 12:39:14 +0900

    feat: restaurant 리스트 카드 작성

---
+++ src/components/company/restaurant/RestaurantList.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="restaurant-list-container">
+        <AddressSelect @restaurants="updateRestaurants" />
+        <div v-if="restaurants.length" class="restaurant-list">
+            <div v-for="restaurant in restaurants" :key="restaurant.id" class="restaurant-item">
+                <h3>{{ restaurant.name }}</h3>
+                <p>{{ restaurant.address }}</p>
+                <p>장당: {{ restaurant.price }}원</p>
+                <button @click="paymentPage(restaurant.id)">식권 구매</button>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+import AddressSelect from '@/components/company/restaurant/AddressSelect.vue';
+  
+export default {
+    components: {
+      AddressSelect,
+    },
+    data() {
+        return {
+            restaurants: [],
+        };
+    },
+    methods: {
+        updateRestaurants(data) {
+            this.restaurants = data;
+        },
+        paymentPage(restaurantId) {
+            this.$router.push(`/restaurant/${restaurantId}/payment`)
+        }
+    },
+};
+</script>
+  
+<style scoped>
+
+.restaurant-list-container {
+    padding: 20px;
+}
+  
+.restaurant-list {
+    display: grid;
+    margin-top: 10px;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    gap: 20px;
+}
+  
+.restaurant-item {
+    border: 1px solid #ccc;
+    padding: 10px;
+    border-radius: 5px;
+background-color: #f9f9f9;
+}
+
+.restaurant-item h3 {
+    margin: 0 0 10px;
+    font-size: 18px;
+}
+
+.restaurant-item p {
+    margin: 0;
+    color: #555;
+}
+</style>
+  
