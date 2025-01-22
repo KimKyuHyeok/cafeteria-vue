b879685755b1c02f71dcb742f457e79f70b6b00d
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-01-22 16:21:46 +0900

    feat: payments 페이지 구현

---
+++ src/views/desktop/company/CompanyPayments.vue
@@ -0,0 +1,22 @@
+<script>
+import CompanyHeaderVue from '@/components/desktop/company/CompanyHeader.vue'
+import PaymentList from '@/components/desktop/company/payments/PaymentList.vue'
+export default {
+    name: 'CompanyPayments',
+    components: {
+        CompanyHeaderVue,
+        PaymentList
+    }
+}
+</script>
+
+
+<template>
+    <header>
+        <CompanyHeaderVue />
+    </header>
+    <PaymentList/>
+</template>
+
+<style scoped>
+</style>
