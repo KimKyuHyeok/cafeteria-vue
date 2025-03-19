57dcb85febfb03f22493271ea6ad7bf3a916e254
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-03-19 17:43:36 +0900

    fix: proxy 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,28 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-		extensions: ['.js', '.vue', '.json'], // 여기에 '.vue' 추가
-	},
-	server: {
-		host: '0.0.0.0',
-		port: 80,
-		fs: {
-			exclude: ['.git']
-		},
-		hmr: {
-			overlay: false
-		},
-	}
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.js', '.vue', '.json'], // 여기에 '.vue' 추가
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 81,
+    fs: {
+      exclude: ['.git'],
+    },
+    hmr: {
+      overlay: false,
+    },
+    proxy: {
+      '/graphql': {
+        target: 'http://api.siggwon-moa.shop',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
